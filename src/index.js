3bfe7e3e1b50b324184ba76bff93b6573f3f50e5
BuildTools <unconfigured@null.spigotmc.org>
2017-02-28 19:31:40 +0900

    commit XD

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const token = 'your token'
+const token = 'your key'
 var msg = 'あああああああああああああああああああああああああああああああ！！！！！！！！！！！（ﾌﾞﾘﾌﾞﾘﾌﾞﾘﾌﾞﾘｭﾘｭﾘｭﾘｭﾘｭﾘｭ！！！！！！ﾌﾞﾂﾁﾁﾌﾞﾌﾞﾌﾞﾁﾁﾁﾁﾌﾞﾘﾘｲﾘﾌﾞﾌﾞﾌﾞﾌﾞｩｩｩｩｯｯｯ！！！！！！！ ）'
 
 client.on('ready', () => {
@@ -15,9 +15,13 @@ client.on('message', message => {
 	} else if (message.content == '!unkov') {
 	    const channel = message.member.voiceChannel;
 
+	    if (channel != undefined && channel.joinable) {
 	    channel.join()
 		.then(connection => console.log('Connected!'))
 		.catch(console.error)
+	    } else {
+		message.reply('You do not have permission to join this voice channel.')
+	    }
 	} else if (message.content == '!d') {
 	    const channel = message.member.voiceChannel;
 
